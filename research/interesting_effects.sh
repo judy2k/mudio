ac441b41ca36457814afcc85d69b06046575bc51
Mark Smith <mark.smith@practicalpoetry.co.uk>
2017-09-16 12:12:39 +0100

    Add some of the interesting Sox effects I've found.

---
+++ research/interesting_effects.sh
@@ -0,0 +1,9 @@
+play mark.mp3 reverse reverb reverse bend 0,-180,0.5
+
+play Sample.mp3 reverse reverb oops gain reverse
+
+play mark.mp3 reverse reverb oops gain reverse bend 0,-500,0.5 gain +10
+
+play mark.mp3 reverse reverb gain reverse bend 0,-500,0.5 gain +10
+
+play Sample.mp3 pitch 100 stretch 1.2 norm dither -S reverse reverb reverse
